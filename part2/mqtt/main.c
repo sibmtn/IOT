c82b644c4437db1c239e05de74bc19bbc5a82e67
sibmtn <79009760+sibmtn@users.noreply.github.com>
2021-03-22 15:40:04 +0100

    Create main.c

---
+++ part2/mqtt/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include "mcp9700.h"
+#include "mqtt.h"
+#include "wifi.h"
+
+static const adc_channel_t channel = ADC_CHANNEL_7; //ADC1 channel 7 GPIO35
+
+void app_main() {
+    wifi_init("BboxbeaWilly_EXT", "martinhouse");
+    
+    mqtt_init("mqtts://iot.devinci.online/", "sm170975", "B!5C#Cxw");
+    
+    mcp9700_init(1,channel);
+    uint32_t mcp_value;
+    while(1) //endless loop
+    {
+        mcp_value = mcp9700_get_value(); 
+        uint32_t mcp_temp = (mcp_value - 500)/10; 
+        char Value[10];
+        sprintf(Value,"%d",mcp_temp);
+
+        mqtt_publish("sm170975/vma311/temp", Value);  
+    }	 
+}
