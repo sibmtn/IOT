b88acef7670e19e32b3f452fbecfdf01fe8d529a
sibmtn <79009760+sibmtn@users.noreply.github.com>
2021-03-22 15:31:29 +0100

    Create main.c

---
+++ part2/adafruit/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include "wifi.h"
+#include "aio.h"
+#include "mcp9700.h"
+
+static const adc_channel_t channel = ADC_CHANNEL_7; //ADC1 channel 7 GPIO35
+
+void app_main() {
+    wifi_init("BboxbeaWilly_EXT", "martinhouse");
+
+    aio_init("sibmartin", "aio_RbIJ04vkGkWvPe6X8WlbGXKg5FxR");
+
+    mcp9700_init(1,channel);
+    uint32_t mcp_value;
+    while(1) //endless loop
+    {
+        mcp_value = mcp9700_get_value(); 
+        uint32_t mcp_temp = (mcp_value - 500)/10; //this function converts the voltage value in degree, it is found in the datasheet of the mcp
+        char ValueIO[10];
+        sprintf(ValueIO,"%d",mcp_temp);
+
+        aio_create_data(ValueIO, "envmon.mcp9700-temp");
+    }	
+}
